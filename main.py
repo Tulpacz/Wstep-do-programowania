f3de64e971ae7ed36483192e6646c965cde0d829
Tulpacz <dawidlaska17@gmail.com>
2023-11-07 00:00:54 +0100

    zadanie 3

---
+++ main.py
@@ -0,0 +1,7 @@
+pierwszy_bok = float(input("Podaj długość pierwszego boku: "))
+drugi_bok = float(input("Podaj długość drugiego boku: "))
+obwod=2*pierwszy_bok+2*drugi_bok
+pole=pierwszy_bok*drugi_bok
+
+print("Obwód prostokąta wynosi: "+str(obwod))
+print("Pole prostokąta wynosi: "+str(pole))
