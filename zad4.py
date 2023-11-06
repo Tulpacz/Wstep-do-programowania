dc54d2a4c456d57253a81d25cb93460ea860452f
Tulpacz <dawidlaska17@gmail.com>
2023-11-07 00:11:25 +0100

    zadanie 4.1

--- zad4.py
+++ zad4.py
@@ -1,4 +1,8 @@
-droga = int(input("Podaj drogę w kilometrach: "))
+import random
+#droga = int(input("Podaj drogę w kilometrach: "))
+
+droga=random.randint(1, 100000)
+
 spalanie = int(input("Podal spalanie w l/100km: "))
 zuzycie=droga*spalanie/100
 koszt=zuzycie*6.5
