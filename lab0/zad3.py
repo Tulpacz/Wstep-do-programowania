2593253e8594056e56537e80574ba2787fa69a0d
Tulpacz <dawidlaska17@gmail.com>
2023-11-07 11:08:28 +0100

    zad 5 wykorzystanie f-string

--- lab0/zad3.py
+++ lab0/zad3.py
@@ -3,5 +3,8 @@ drugi_bok = float(input("Podaj długość drugiego boku: "))
 obwod=2*pierwszy_bok+2*drugi_bok
 pole=pierwszy_bok*drugi_bok
 
-print("Obwód prostokąta wynosi: "+str(obwod))
-print("Pole prostokąta wynosi: "+str(pole))
+#print("Obwód prostokąta wynosi: "+str(obwod))
+#print("Pole prostokąta wynosi: "+str(pole))
+
+print(f"Obwód prostokąta wynosi: {obwod}")
+print(f"Pole prostokąta wynosi: {pole}")
--- lab0/zad4.py
+++ lab0/zad4.py
@@ -1,4 +1,6 @@
 import random
+
+#ręczne podawanie drogi
 #droga = int(input("Podaj drogę w kilometrach: "))
 
 droga=random.randint(1, 100000)
@@ -7,6 +9,8 @@ spalanie = int(input("Podal spalanie w l/100km: "))
 zuzycie=droga*spalanie/100
 koszt=zuzycie*6.5
 
-print("Zużycie paliwa wyniesie: "+str(zuzycie))
+#print("Zużycie paliwa wyniesie: "+str(zuzycie))
+print(f'Zużycie paliwa wyniesie: {zuzycie}')
 
-print("Koszt podróży wyniesie:"+str(koszt))
+#print("Koszt podróży wyniesie:"+str(koszt))
+print(f"Koszt podróży wyniesie: {koszt}")
