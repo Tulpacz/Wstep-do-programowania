c463b52d5fa46f4c1387f15397363fecf45a6b1d
w70934 <dawidlaska17@gmail.com>
2023-12-10 16:43:43 +0100

    zad1

---
+++ lab3/zad1a.py
@@ -0,0 +1,13 @@
+#a
+lista = ["Dawid", "Adam","Jan","Janusz"]
+posortowana = sorted(lista)
+print(posortowana)
+#b
+lista.append(["Michał","Mateusz"])
+ostatnia = lista.pop()
+#c
+lista.insert(3,"Andrzej")
+#d
+lista.reverse()
+lista=lista*2
+#print(lista)
